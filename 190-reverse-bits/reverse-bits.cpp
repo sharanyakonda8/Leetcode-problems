bba109e680c4b8d84e78b2a45912cdb56b2fbab2
sharanyakonda8 <sharanyakonda8@gmail.com>
2026-08-01 17:12:41 +0530

    Time: 2 ms (36.77%) | Memory: 8.7 MB (5.36%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+      uint32_t a=0;
+        vector<int>d;
+        for(int i=0;i<32;i++){
+            d.push_back(n&1);
+            n>>=1;
+        }
+        for(int i=0;i<32;i++){
+           a=a*2+d[i];
+        }
+        return a;  
+    }
+};
